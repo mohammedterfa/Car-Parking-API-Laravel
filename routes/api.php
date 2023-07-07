95ecd267ee1f0817e6d232991b555b153d440bc8
mohammedterfa <mohammedterfa@gmail.com>
2023-07-07 14:37:35 +0200

    get all zones

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use \App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\ZoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('auth/register', Auth\RegisterController::class);
 
 Route::post('auth/login', Auth\LoginController::class);
+
+Route::get('zones', [ZoneController::class, 'index']);
 
 
 Route::middleware('auth:sanctum')->group(function(){
