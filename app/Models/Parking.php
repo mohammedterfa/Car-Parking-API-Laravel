03efb7025ea102457d716cc1953d03af8f68ae34
mohammedterfa <mohammedterfa@gmail.com>
2023-07-15 22:42:02 +0200

    added globle scope to Parking model

--- app/Models/Parking.php
+++ app/Models/Parking.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -40,5 +41,12 @@ class Parking extends Model
     public function scopeStopped($query)
     {
         return $query->whereNotNull('stop_time');
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope('user', function (Builder $builder){
+            $builder->where('user_id', auth()->id());
+        });
     }
 }
